9bfbaa825aaa1b00a15cff2bf74d94344cce5b5e
Abhilash <Abhilash>
2021-12-31 23:54:57 +0530

    Initial commit

---
+++ src/main/java/S1.java
@@ -0,0 +1,71 @@
+public class S1 {
+    public static void main(String[] args) {
+
+        //1.Aunit test is apiee of code written by developer
+        //to test certain funcironlity of code
+        //2.The class which we want to test is called
+        //as System/Class underTest;
+        //3. The method which we want to test is called
+        //as Method under test
+        //4. Unit test instantiates class under test,executes
+        //method under test and verifies that it worked
+        //as expected
+
+
+       //what makes a good unit test?
+
+         //Easy to write
+         //readable
+        //reliable
+        //fast
+        //isolated
+
+        //what is junit
+        //simple is a simple,open source framwork
+        //to write and run repeateable tasks;
+        //junit is a java testing framework used to test a piee
+        //of java code (Code under test or System under test)
+        //it has a runner which determines the tests, executes those test,
+        //reports the results.
+        //junit test validates expected result with acutal output
+        //from the code under test.
+        //thus,based on assertions success or failure it provide
+        //feedback to developer
+
+
+        //problems  with junit 4
+        //junit.jar was single fat jar
+         //no flexible api
+        //no java 8 features
+        //no extension point
+
+
+
+        //soulutio with junit5
+
+        //multiple small jars
+        // flexible api
+        //java 8 feautes
+        //extendsion points
+
+
+        //sepearation of concers(junit 5)
+        //an api for developer to write test
+        //mecahnism to discover and execute test
+        //an api for externsal to run tests in their environment
+
+
+        //junit 5 components
+
+        //junit is majorioy divided into 3 main components
+        /// junit=junit platform +junit+junit vintageg
+
+//architeture
+        //platform
+        //jupiter--vintage--custom api
+
+
+
+
+    }
+}
